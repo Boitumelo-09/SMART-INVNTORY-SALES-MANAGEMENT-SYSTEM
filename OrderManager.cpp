afd3c7fbf4620c0c718e32338704762c71e4841c
boitumelo-09 <belmiredub4@gmail.com>
2025-09-04 20:59:21 +0200

    feat : improved and introduced some management methods to the order

--- OrderManager.cpp
+++ OrderManager.cpp
@@ -1,1 +1,7 @@
 #include "OrderManager.h"
+
+
+const std::vector<Order> OrderManager::getOrders() { return orders; }
+void OrderManager::createOrder(){}
+void OrderManager::displayAllOrders(){}
+void OrderManager::displayCustomerOrderSummary(){}
--- OrderManager.h
+++ OrderManager.h
@@ -1,7 +1,18 @@
 #pragma once
+#include "utility.h"
+#include "Order.h"
+#include "Customer.h"
+#include "Product.h"
+#include "vector"
 class OrderManager
 
 {
+	std::vector<Order> orders;
+public:
+	const std::vector<Order> getOrders();
+	void createOrder();
+	void displayAllOrders();
+	void displayCustomerOrderSummary();
 
 };
 

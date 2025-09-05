682cfcaaf54ec1ae1e78591a70f9a0d61d08cf54
boitumelo-09 <belmiredub4@gmail.com>
2025-09-05 21:13:01 +0200

    UPDATE : added a system engine to connect all operations

---
+++ SystemEngine.cpp
@@ -0,0 +1,11 @@
+#include "SystemEngine.h"
+#include "ProductManager.h"
+#include "CustomerManager.h"
+#include "OrderManager.h"
+#include "Order.h"
+#include "Product.h"
+#include "Customer.h"
+#include <iostream>
+
+
+
---
+++ SystemEngine.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "ProductManager.h"
+#include "CustomerManager.h"
+#include "OrderManager.h"
+#include "Order.h"
+#include "Product.h"
+#include "Customer.h"
+#include <iostream>
+
+class SystemEngine
+{
+private:
+
+};
+

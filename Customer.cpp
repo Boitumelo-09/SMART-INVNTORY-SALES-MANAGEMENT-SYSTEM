4d38801655d9cfd2c34ffd50f341170904d3e05a
boitumelo-09 <belmiredub4@gmail.com>
2025-09-09 19:52:30 +0200

    fix: updated constructor

--- Customer.cpp
+++ Customer.cpp
@@ -3,8 +3,9 @@
 #include <string>
 #include <iostream>
 
-
-Customer::Customer(int customerID, std::string& customerName, std::string& customerEmail, std::string& deliveryAddress): customerID(customerID) , customerName(customerName) , customerEmail(customerEmail) , deliveryAddress(deliveryAddress){}
+Customer::Customer(int customerID, const std::string& customerName, const std::string& customerEmail, const std::string& deliveryAddress)
+    : customerID(customerID), customerName(customerName), customerEmail(customerEmail), deliveryAddress(deliveryAddress) {
+}
 
 int Customer::getCustomerId() const { return customerID; }
 std::string Customer::getCustomerName() const { return customerName; }
@@ -13,25 +14,24 @@ std::string Customer::getCustomerAddress() const { return deliveryAddress; }
 
 void Customer::setCustomerName(const std::string& newName) { customerName = newName; }
 void Customer::setCustomerEmail(const std::string& newEmail) { customerEmail = newEmail; }
-void Customer::setCustomerAddress(const std::string& newAddress) {  deliveryAddress = newAddress; }
-
+void Customer::setCustomerAddress(const std::string& newAddress) { deliveryAddress = newAddress; }
 
 void Customer::displayCustomer() const {
-	clearScreen();
-	verticalPadding();
-	std::cout << horizontalPadding() << "Customer Information";
-	newLine();  
-	std::cout << horizontalPadding() << "CUSTOMER ID      : " << getCustomerId();
-	newLine();
-	std::cout << horizontalPadding() << "CUSTOMER NAME    : " << getCustomerName();
-	newLine();
-	std::cout << horizontalPadding() << "EMAIL ADDRESS    : " << getCustomerEmail();
-	newLine(); 
-	std::cout << horizontalPadding() << "DELIVERY ADDRESS : " << getCustomerAddress();
-	newLine();
-	std::cout << horizontalPadding() << "*********************************************" << getCustomerAddress();
-	newLine();
-	newLine();
-	pressToContinue();
-	return;	
+    clearScreen();
+    verticalPadding();
+    std::cout << horizontalPadding() << "Customer Information";
+    newLine();
+    std::cout << horizontalPadding() << "CUSTOMER ID      : " << getCustomerId();
+    newLine();
+    std::cout << horizontalPadding() << "CUSTOMER NAME    : " << getCustomerName();
+    newLine();
+    std::cout << horizontalPadding() << "EMAIL ADDRESS    : " << getCustomerEmail();
+    newLine();
+    std::cout << horizontalPadding() << "DELIVERY ADDRESS : " << getCustomerAddress();
+    newLine();
+    std::cout << horizontalPadding() << "*********************************************";
+    newLine();
+    newLine();
+    pressToContinue();
+    return;
 }

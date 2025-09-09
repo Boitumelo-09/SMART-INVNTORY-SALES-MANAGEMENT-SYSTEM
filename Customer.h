d66a5e7668f10ddfb1e38d54d88eba10b7294680
boitumelo-09 <belmiredub4@gmail.com>
2025-09-09 19:51:16 +0200

    fix : fixedcode style

--- Customer.h
+++ Customer.h
@@ -3,21 +3,20 @@
 
 class Customer
 {
-	int customerID;
-	std::string customerName;
-	std::string customerEmail;
-	std::string deliveryAddress;
+    int customerID;
+    std::string customerName;
+    std::string customerEmail;
+    std::string deliveryAddress;
 public:
-	Customer(int customerID, std::string& customerName, std::string& customerEmail, std::string& deliveryAddress);
+    Customer(int customerID, const std::string& customerName, const std::string& customerEmail, const std::string& deliveryAddress);
 
     int getCustomerId() const;
     std::string getCustomerName() const;
     std::string getCustomerEmail() const;
     std::string getCustomerAddress() const;
-  
+
     void setCustomerName(const std::string& newName);
     void setCustomerEmail(const std::string& newEmail);
     void setCustomerAddress(const std::string& newAddress);
     void displayCustomer() const;
 };
-
